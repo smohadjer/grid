8de27e181398dcc2a0cb60d003adab9950cd455b
Saeid Mohadjer <saeid@fastmail.fm>
2016-01-10 09:41:10 +0100

    added minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
 
 gulp.task('sass', function () {
 	gulp.src('./*.scss')
 	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./dist'))
+	.pipe(cssnano())
+	.pipe(concat('grid.min.css'))
 	.pipe(gulp.dest('./dist'));
 });
 
